3be9c91b59035358c8611f031012b2bf359f50b0
ranjeet <ranjeet@email.com>
2024-02-05 23:30:46 +0530

    commited modified-2 main file

--- main.tf
+++ main.tf
@@ -64,23 +64,23 @@ resource "aws_iam_role" "node-role" {
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.role.name
+  role       = aws_iam_role.node-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.role.name
+  role       = aws_iam_role.node-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.role.name
+  role       = aws_iam_role.node-role.name
 }
 # node creation
-resource "aws_eks_node_group" "node" {
+resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "example"
-  node_role_arn   = aws_iam_role.role.arn
+  node_group_name = "node"
+  node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = data.aws_subnets.public.ids
 
   scaling_config {
